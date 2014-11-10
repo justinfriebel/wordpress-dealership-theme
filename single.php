adbd46ed39de4d06f59dae8f2e03b0fdbd4602b4
Justin Friebel <friebj99@gmail.com>
2014-11-10 16:13:04 -0600

    tabs to spaces single.php

--- single.php
+++ single.php
@@ -6,30 +6,30 @@
  */
 
 get_header(); ?>
-	
-	<div class="row" data-equalizer><!-- Foundation .row start -->
-		<div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+  <div class="row" data-equalizer><!-- Foundation .row start -->
+    <div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-					<?php get_template_part( 'content', 'single' ); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php // wordpress_dealership_post_nav(); ?>
+          <?php get_template_part( 'content', 'single' ); ?>
 
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
+          <?php // wordpress_dealership_post_nav(); ?>
 
-				<?php endwhile; // end of the loop. ?>
+          <?php
+            // If comments are open or we have at least one comment, load up the comment template
+            if ( comments_open() || '0' != get_comments_number() ) :
+              comments_template();
+            endif;
+          ?>
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div><!-- Foundation .columns end -->
-	</div><!-- Foundation .row end -->
-	
+        <?php endwhile; // end of the loop. ?>
+
+        </main><!-- #main -->
+      </div><!-- #primary -->
+    </div><!-- Foundation .columns end -->
+  </div><!-- Foundation .row end -->
+
 <?php get_footer(); ?>

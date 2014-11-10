92e269184bb79b01d5db0833559dddec7b9bb088
Justin Friebel <friebj99@gmail.com>
2014-11-10 15:33:35 -0600

    tabs to spaces home.php

--- home.php
+++ home.php
@@ -19,43 +19,43 @@ get_header(); ?>
     </div><!-- Foundation .row end -->
   </div>
 
-	<div class="row post-listing-padding" data-equalizer><!-- Foundation .row start -->
-		<div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+  <div class="row post-listing-padding" data-equalizer><!-- Foundation .row start -->
+    <div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-					<?php if ( have_posts() ) : ?>
+          <?php if ( have_posts() ) : ?>
 
-						<ul class="medium-block-grid-2 large-block-grid-2"><!-- Foundation block grid start -->
+            <ul class="medium-block-grid-2 large-block-grid-2"><!-- Foundation block grid start -->
 
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
 
-							<li><!-- Foundation block grid item start -->
-							<?php
-								/* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'content', get_post_format() );
-							?>
-							</li><!-- Foundation block grid item end -->
+              <li><!-- Foundation block grid item start -->
+              <?php
+                /* Include the Post-Format-specific template for the content.
+                * If you want to override this in a child theme, then include a file
+                * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                */
+                get_template_part( 'content', get_post_format() );
+              ?>
+              </li><!-- Foundation block grid item end -->
 
-						<?php endwhile; ?>
+            <?php endwhile; ?>
 
-						</ul><!-- Foundation block grid end -->
+            </ul><!-- Foundation block grid end -->
 
-						<?php wordpress_dealership_paging_nav(); ?>
+            <?php wordpress_dealership_paging_nav(); ?>
 
-					<?php else : ?>
+          <?php else : ?>
 
-						<?php get_template_part( 'content', 'none' ); ?>
+            <?php get_template_part( 'content', 'none' ); ?>
 
-					<?php endif; ?>
+          <?php endif; ?>
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div><!-- Foundation .columns end -->
-	</div><!-- Foundation .row end -->
+        </main><!-- #main -->
+      </div><!-- #primary -->
+    </div><!-- Foundation .columns end -->
+  </div><!-- Foundation .row end -->
 
 <?php get_footer(); ?>

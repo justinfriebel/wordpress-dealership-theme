8dc2215d455ede45c9135442082a040151c50c6b
Justin Friebel <friebj99@gmail.com>
2014-11-10 13:51:29 -0600

    tabs to spaces for content-single.php

--- content-single.php
+++ content-single.php
@@ -5,34 +5,34 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+  <header class="entry-header">
     
-		<?php
-
+    <?php
+      
       if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
         the_post_thumbnail( 'full' );
       }
 
       the_title( '<h1 class="entry-title">', '</h1>' );
-    
+      
     ?>
 
-		<div class="entry-meta">
-			<?php wordpress_dealership_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+    <div class="entry-meta">
+      <?php wordpress_dealership_posted_on(); ?>
+    </div><!-- .entry-meta -->
+  </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'wordpress-dealership' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+  <div class="entry-content">
+    <?php the_content(); ?>
+    <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . __( 'Pages:', 'wordpress-dealership' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+  </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'wordpress-dealership' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+  <footer class="entry-footer">
+    <?php edit_post_link( __( 'Edit', 'wordpress-dealership' ), '<span class="edit-link">', '</span>' ); ?>
+  </footer><!-- .entry-footer -->
 </article><!-- #post-## -->

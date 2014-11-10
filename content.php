de6b7b82eff1ae5a0db2a37429e2cce2a72a815f
Justin Friebel <friebj99@gmail.com>
2014-11-10 13:48:48 -0600

    tabs to spaces for content.php

--- content.php
+++ content.php
@@ -5,35 +5,35 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
+  <header class="entry-header">
+    <?php
       
       if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-        
+      
     ?>
-        <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+    
+    <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
     
     <?php }
       
       the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-    
+      
     ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php // wordpress_dealership_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+    <?php if ( 'post' == get_post_type() ) : ?>
+    <div class="entry-meta">
+      <?php // wordpress_dealership_posted_on(); ?>
+    </div><!-- .entry-meta -->
+    <?php endif; ?>
+  </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wordpress-dealership' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'wordpress-dealership' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
+  <div class="entry-content">
+    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wordpress-dealership' ) ); ?>
+    <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . __( 'Pages:', 'wordpress-dealership' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+  </div><!-- .entry-content -->
 </article><!-- #post-## -->

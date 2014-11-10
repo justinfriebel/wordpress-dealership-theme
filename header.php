b8184cbe087932a4f4a09d6ec79773996a6d184b
Justin Friebel <friebj99@gmail.com>
2014-11-10 11:43:12 -0600

    update tabs to spaces for header

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The header for our theme.
+* The header for our theme.
  *
  * Displays all of the <head> section and everything up till <div id="content">
  *
@@ -23,59 +23,52 @@
 <!-- Foundation .off-canvas-wrap start -->
 
 <div class="off-canvas-wrap" data-offcanvas>
-	<div class="inner-wrap">
+  <div class="inner-wrap">
 
-	<nav class="tab-bar hide-for-large-up">
+  	<nav class="tab-bar hide-for-large-up">
     <section class="left-small">
       <a class="left-off-canvas-toggle menu-icon" ><span></span></a>
     </section>
     <a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/dev-images/wordpress-dealership-theme.png" class="text-center mobile-logo" /></a>
   </nav>
 
-	<!-- Off Canvas Menu -->
-	<aside class="left-off-canvas-menu">
-		<ul>
-			<?php wp_nav_menu( array( 
-				'theme_location' => 'primary', 
-				'container' => '', 
-				'menu_class' => '', 
-				'menu_id' => '', 
-				'items_wrap' => '%3$s',
-				'depth' => 1
-			) ); ?>
-		</ul>
-	</aside>
+  <!-- Off Canvas Menu -->
+  <aside class="left-off-canvas-menu">
+    <ul>
+      <?php wp_nav_menu( array( 
+        'theme_location' => 'primary', 
+        'container' => '', 
+        'menu_class' => '', 
+        'menu_id' => '', 
+        'items_wrap' => '%3$s',
+        'depth' => 1
+      ) ); ?>
+    </ul>
+  </aside>
 
 <!-- Foundation .off-canvas-wrap end -->
 
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wordpress-dealership' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wordpress-dealership' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">    
+  <header id="masthead" class="site-header" role="banner">    
     <!-- Foundation top-bar navigation start -->
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<div class="row show-for-large-up header-padding">
-				<div class="small-12 columns">
+    <nav id="site-navigation" class="main-navigation" role="navigation">
+      <div class="row show-for-large-up header-padding">
+        <div class="small-12 columns">
           <a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/dev-images/wordpress-dealership-theme.png" class="left" /></a>
-					<nav class="top-bar hide-for-small right" data-topbar>
-						<section class="top-bar-section right">
-						<?php wp_nav_menu( array( 
-							'theme_location' => 'primary', 
-							'menu_id' => 'top-nav',
-						) ); ?>
-						</section>
-					</nav><!-- #site-navigation -->
-				</div>
-			</div>
-		</nav><!-- .row -->
-		<!-- Foundation top-bar navigation end -->
+          <nav class="top-bar hide-for-small right" data-topbar>
+            <section class="top-bar-section right">
+              <?php wp_nav_menu( array( 
+                'theme_location' => 'primary', 
+                'menu_id' => 'top-nav',
+              ) ); ?>
+            </section>
+          </nav><!-- #site-navigation -->
+        </div>
+      </div>
+    </nav><!-- .row -->
+    <!-- Foundation top-bar navigation end -->
+  </header><!-- #masthead -->
 
-    <?php /*
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'wordpress-dealership' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-    */ ?>
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+  <div id="content" class="site-content">

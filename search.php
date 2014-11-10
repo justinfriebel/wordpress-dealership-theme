b783221ea3d1b4f63407d16a1649e8b0d3111799
Justin Friebel <friebj99@gmail.com>
2014-11-10 16:09:16 -0600

    tabs to spaces search.php

--- search.php
+++ search.php
@@ -7,47 +7,46 @@
 
 get_header(); ?>
 
-	<div class="row" data-equalizer><!-- Foundation .row start -->
-		<div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
-			<section id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+  <div class="row" data-equalizer><!-- Foundation .row start -->
+    <div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
+      <section id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-				<?php if ( have_posts() ) : ?>
+        <?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
-						<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'awesome-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-					</header><!-- .page-header -->
+          <header class="page-header">
+            <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'awesome-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+          </header><!-- .page-header -->
 
-					<?php /* Start the Loop */ ?>
-					<ul class="large-block-grid-2"><!-- Foundation block grid start -->
-					<?php while ( have_posts() ) : the_post(); ?>
+          <?php /* Start the Loop */ ?>
+          <ul class="large-block-grid-2"><!-- Foundation block grid start -->
+          <?php while ( have_posts() ) : the_post(); ?>
 
-						<li><!-- Foundation block grid item start -->
-						<?php
-						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
-						get_template_part( 'content', 'search' );
-						?>
-						</li><!-- Foundation block grid item end -->
+            <li><!-- Foundation block grid item start -->
+            <?php
+            /**
+            * Run the loop for the search to output the results.
+            * If you want to overload this in a child theme then include a file
+            * called content-search.php and that will be used instead.
+            */
+            get_template_part( 'content', 'search' );
+            ?>
+            </li><!-- Foundation block grid item end -->
 
-					<?php endwhile; ?>
-					</ul><!-- Foundation block grid end -->
+          <?php endwhile; ?>
+          </ul><!-- Foundation block grid end -->
 
-					<?php awesome_theme_paging_nav(); ?>
+          <?php awesome_theme_paging_nav(); ?>
 
-				<?php else : ?>
+        <?php else : ?>
 
-					<?php get_template_part( 'content', 'none' ); ?>
+          <?php get_template_part( 'content', 'none' ); ?>
 
-				<?php endif; ?>
+        <?php endif; ?>
 
-				</main><!-- #main -->
-			</section><!-- #primary -->
+        </main><!-- #main -->
+      </section><!-- #primary -->
+    </div><!-- Foundation .columns end -->
+  </div><!-- Foundation .row end -->
 
-		</div><!-- Foundation .columns end -->
-	</div><!-- Foundation .row end -->
-	
 <?php get_footer(); ?>
